f8ccccaaae34677a6d875b2442348e678fb9fa86
liyinrong <li.yinrong.acc@outlook.com>
2024-01-05 01:29:12 +0100

    implement resnet18 for final test.

--- cnn.h
+++ cnn.h
@@ -6,7 +6,7 @@
 **************************************************************************************************/
 
 /*
- * This header file was automatically @generated for the RTDemo_MAX_alpha network from a template.
+ * This header file was automatically @generated for the RTDemo_MAX_alpha_resnet18 network from a template.
  * Please do not edit; instead, edit the template and regenerate.
  */
 
@@ -23,35 +23,40 @@ typedef int16_t q15_t;
 
 /*
   SUMMARY OF OPS
-  Hardware: 746,496 ops (720,384 macc; 19,968 comp; 6,144 add; 0 mul; 0 bitwise)
+  Hardware: 1,351,264 ops (1,322,624 macc; 19,872 comp; 8,768 add; 0 mul; 0 bitwise)
     Layer 0 (conv1): 55,296 ops (55,296 macc; 0 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 1 (pool1): 3,072 ops (0 macc; 3,072 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 2 (convblk1_convr): 99,840 ops (98,304 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 3 (convblk1_conv1): 24,960 ops (24,576 macc; 384 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 4 (convblk1_conv2): 18,816 ops (18,432 macc; 384 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 5 (convblk1_conv3): 26,112 ops (24,576 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 6 (convblk1_resid): 1,536 ops (0 macc; 0 comp; 1,536 add; 0 mul; 0 bitwise)
-    Layer 7 (convblk2_convr): 99,840 ops (98,304 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 8 (convblk2_conv1): 24,960 ops (24,576 macc; 384 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 9 (convblk2_conv2): 18,816 ops (18,432 macc; 384 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 10 (convblk2_conv3): 26,112 ops (24,576 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 11 (convblk2_resid): 1,536 ops (0 macc; 0 comp; 1,536 add; 0 mul; 0 bitwise)
-    Layer 12 (convblk3_convr): 99,840 ops (98,304 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 13 (convblk3_conv1): 24,960 ops (24,576 macc; 384 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 14 (convblk3_conv2): 18,816 ops (18,432 macc; 384 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 15 (convblk3_conv3): 26,112 ops (24,576 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 16 (convblk3_resid): 1,536 ops (0 macc; 0 comp; 1,536 add; 0 mul; 0 bitwise)
-    Layer 17 (convblk4_convr): 99,840 ops (98,304 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 18 (convblk4_conv1): 24,960 ops (24,576 macc; 384 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 19 (convblk4_conv2): 18,816 ops (18,432 macc; 384 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 20 (convblk4_conv3): 26,112 ops (24,576 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 21 (convblk4_resid): 1,536 ops (0 macc; 0 comp; 1,536 add; 0 mul; 0 bitwise)
-    Layer 22 (pool2): 1,536 ops (0 macc; 1,536 comp; 0 add; 0 mul; 0 bitwise)
-    Layer 23 (fc): 1,536 ops (1,536 macc; 0 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 1 (conv2): 565,248 ops (565,248 macc; 0 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 2 (pool1): 2,944 ops (0 macc; 2,944 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 3 (convblk1_convr): 95,680 ops (94,208 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 4 (convblk1_conv1): 23,920 ops (23,552 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 5 (convblk1_conv2): 18,032 ops (17,664 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 6 (convblk1_conv3): 25,024 ops (23,552 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 7 (convblk1_resid): 1,472 ops (0 macc; 0 comp; 1,472 add; 0 mul; 0 bitwise)
+    Layer 8 (convblk2_convr): 95,680 ops (94,208 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 9 (convblk2_conv1): 23,920 ops (23,552 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 10 (convblk2_conv2): 18,032 ops (17,664 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 11 (convblk2_conv3): 25,024 ops (23,552 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 12 (convblk2_resid): 1,472 ops (0 macc; 0 comp; 1,472 add; 0 mul; 0 bitwise)
+    Layer 13 (identityblk1_pass): 0 ops (0 macc; 0 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 14 (identityblk1_conv1): 23,920 ops (23,552 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 15 (identityblk1_conv2): 18,032 ops (17,664 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 16 (identityblk1_conv3): 25,024 ops (23,552 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 17 (identityblk1_resid): 1,472 ops (0 macc; 0 comp; 1,472 add; 0 mul; 0 bitwise)
+    Layer 18 (convblk3_convr): 95,680 ops (94,208 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 19 (convblk3_conv1): 23,920 ops (23,552 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 20 (convblk3_conv2): 18,032 ops (17,664 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 21 (convblk3_conv3): 25,024 ops (23,552 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 22 (convblk3_resid): 1,472 ops (0 macc; 0 comp; 1,472 add; 0 mul; 0 bitwise)
+    Layer 23 (convblk4_convr): 95,680 ops (94,208 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 24 (convblk4_conv1): 23,920 ops (23,552 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 25 (convblk4_conv2): 18,032 ops (17,664 macc; 368 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 26 (convblk4_conv3): 25,024 ops (23,552 macc; 1,472 comp; 0 add; 0 mul; 0 bitwise)
+    Layer 27 (convblk4_resid_pool2): 2,880 ops (0 macc; 0 comp; 2,880 add; 0 mul; 0 bitwise)
+    Layer 28 (fc): 1,408 ops (1,408 macc; 0 comp; 0 add; 0 mul; 0 bitwise)
 
   RESOURCE USAGE
-  Weight memory: 30,336 bytes out of 442,368 bytes total (6.9%)
-  Bias memory:   704 bytes out of 2,048 bytes total (34.4%)
+  Weight memory: 45,312 bytes out of 442,368 bytes total (10.2%)
+  Bias memory:   864 bytes out of 2,048 bytes total (42.2%)
 */
 
 /* Number of outputs for this network */
